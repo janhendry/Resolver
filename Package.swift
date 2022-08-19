c006aad0c9b8c5189ce2897f4a9a801c9c370a6b
Jan Anstipp <j.anstipp@me.com>
2022-08-19 10:20:15 +0200

    remove platform

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Resolver",
-    platforms: [
-        .iOS(.v11),
-        .macOS(.v10_14),
-        .tvOS(.v13),
-        .watchOS(.v6)
-    ],
     products: [
         .library(
             name: "Resolver",
